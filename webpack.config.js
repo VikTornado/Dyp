2fa3e691f1e74551c9b2c36e3f03dbc9b4969bd6
Glo_study <3809164@gmail.com>
2022-05-20 20:51:22 +0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: ".src/index.js",
-  output: {
-    filename: ".js/main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    hot: true,
-    static: {
-      directory: "./dist",
-      watch: true,
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'dist')
     },
-  },
-};
+    devServer: {
+        hot: true,
+        static: {
+            directory: './dist',
+            watch: true
+        }
+    }
+}
