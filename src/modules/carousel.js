d4fa2f048e7203195a757cfbd76891663ebee917
Glo_study <3809164@gmail.com>
2022-05-23 11:45:34 +0300

    carousel

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,22 +1,28 @@
 import Swiper, { Navigation } from "swiper";
 
 const carousel = function () {
-  const swiper = new Swiper(".swiper", {
+
+const swiper = new Swiper(".swiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
     slidesPerGroup: 3,
     modules: [Navigation],
     breakpoints: {
+      576: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
       768: {
         slidesPerView: 2,
+        slidesPerGroup: 2,
       },
+  
       1024: {
         slidesPerView: 3,
       },
     },
     loop: true,
     loopFillGroupWithBlank: true,
-
+  
     navigation: {
       nextEl: ".arrow-right",
       prevEl: ".arrow-left",
